7b0d5a5d08d3db94842e904ca0b4aa0b25eca66c
Dalia <106385824+daliauv1@users.noreply.github.com>
2022-07-26 14:06:06 +0200

    Update Support.jsx

--- src/views/support/Support.jsx
+++ src/views/support/Support.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Form from '../../components/contactUs/Form';
 import Navbar from '../../components/navbar/Navbar';
-import '../support/support.scss';
 
 function Support() {
   return (
