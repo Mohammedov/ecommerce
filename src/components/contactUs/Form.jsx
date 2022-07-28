9547a01b0dbc98821496907dc17e9819686be283
Dalia <dalia.dmuv@gmail.com>
2022-07-28 14:50:31 +0200

    Pull to get navbar actualiced

--- src/components/contactUs/Form.jsx
+++ src/components/contactUs/Form.jsx
@@ -1,32 +1,36 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './form.scss'
 
 
+
 function Form(){
   const form = useRef();
 
+
   const sendEmail = (e) => {
     e.preventDefault();
-
     emailjs.sendForm('service_tw4oukr', 'template_xa2yifb', form.current, 'D3fP7M862KGnni-Tv')
-      .then((result) => {
-          console.log(result.text);
+  .then((result) => {
+      console.log(result.text);
+     
       }, (error) => {
           console.log(error.text);
       });
+     
   };
+
 
   return (
     <div className="contact-us">
-    <form ref={form} onSubmit={sendEmail}>
+    <form ref={form} onSubmit={sendEmail} id="Form">
       <h1 className="d-flex justify-content-center">
         Atención al cliente
         </h1>
       <div className="form-group">
         <label>Nombre</label>
-        <input type="name" className="form-control" id="user_name" name="name"  required/>
+        <input type="name" className="form-control" id="user_name" name="name" required/>
       </div>
       <div className="form-group">
         <label>Apellido</label>
@@ -38,12 +42,12 @@ function Form(){
       </div>  
       <div className="form-group">
         <label>Mensaje</label>
-        <textarea className="form-control" rows="3" name="message" required/>
+        <textarea className="form-control" rows="3" name="message" required />
         <br></br>  
       </div>
-      <div className="form-group d-flex justify-content-center" >
-        <button  type="button" className="btn btn-danger">  
-           <input type="submit" className= "buttonSend d-flex align-self-center" value="Enviar" />
+      <div className="form-group d-flex justify-content-center"  >
+        <button onClick={sendEmail} type="button" className="btn btn-danger" >  
+           <input type="submit" className= "buttonSend d-flex align-self-center" value="Enviar"   />
         </button>  
       </div>
     </form>
